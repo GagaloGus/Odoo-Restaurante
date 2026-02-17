566105fd603a88e80eafeab94f8b671a37879587
Sole <soledadmoral13@gmail.com>
2026-02-17 23:10:04 +0100

    todo correcto en categorias, falta mejorarlo

--- addons/modulo_categorias/__manifest__.py
+++ addons/modulo_categorias/__manifest__.py
@@ -13,14 +13,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'website'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/modulo_categorias_views.xml',
         'views/modulo_categorias_menu.xml',
-        #'views/templates.xml',
+        'views/templates.xml',
     ],
     'application': True,
     'installable': True
--- addons/modulo_categorias/controllers/controllers.py
+++ addons/modulo_categorias/controllers/controllers.py
@@ -6,4 +6,9 @@ from odoo.http import request
 class ModuloCategorias(http.Controller):
      @http.route('/categorias', auth='public', website=True)
      def categorias_list(self, **kwargs):
-         return "Hello, world"
+         
+        categorias = request.env['restaurante.categoria'].sudo().search([('active', '=', True)])
+
+        return request.render('modulo_categorias.template_categorias', {
+            'categorias': categorias
+        })
